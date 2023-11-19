a74d28209c15c8964a6d5b38ba7a96615a5d91f0
Nabil <lahlalia.nabil@hotmail.com>
2023-11-19 14:06:24 +0100

    implements jwt on authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AuthComponent } from './auth/auth.component';
+import { WelcomeContentComponent } from './welcome-content/welcome-content.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { ContentComponent } from './content/content.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent
+    AuthComponent,
+    WelcomeContentComponent,
+    LoginFormComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
